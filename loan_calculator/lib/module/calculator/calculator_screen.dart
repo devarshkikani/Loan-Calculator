a2598043d5fb9e99a50ec5159583f3a5fd889fa8
devarsh kikani <sahilr96649@gmail.com>
2024-02-24 01:25:54 +0530

    add calculator

--- loan_calculator/lib/module/calculator/calculator_screen.dart
+++ loan_calculator/lib/module/calculator/calculator_screen.dart
@@ -1,4 +1,8 @@
 import 'package:flutter/material.dart';
+import 'package:loan_calculator/constants/app_text.dart';
+import 'package:loan_calculator/theme/app_colors.dart';
+import 'package:loan_calculator/theme/app_text_style.dart';
+import 'package:math_expressions/math_expressions.dart';
 
 class CalculatorScreen extends StatefulWidget {
   const CalculatorScreen({super.key});
@@ -8,8 +12,253 @@ class CalculatorScreen extends StatefulWidget {
 }
 
 class _CalculatorScreenState extends State<CalculatorScreen> {
+  var userInput = '';
+  var answer = '';
+
+  // Array of button
+  final List<String> buttons = [
+    'AC',
+    'DEL',
+    '%',
+    '/',
+    '7',
+    '8',
+    '9',
+    'x',
+    '4',
+    '5',
+    '6',
+    '-',
+    '1',
+    '2',
+    '3',
+    '+',
+    '+/-',
+    '0',
+    '.',
+    '=',
+  ];
+
   @override
   Widget build(BuildContext context) {
-    return const Scaffold();
+    return Scaffold(
+      appBar: AppBar(
+        title: Text(
+          AppText.calculator,
+          style: AppTextStyle.semiBold22,
+        ),
+      ),
+      body: Column(
+        mainAxisAlignment: MainAxisAlignment.end,
+        children: <Widget>[
+          const Expanded(flex: 1, child: SizedBox()),
+          Column(
+            mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+            children: <Widget>[
+              Container(
+                padding: const EdgeInsets.all(20),
+                alignment: Alignment.centerRight,
+                child: Text(
+                  userInput,
+                  style: const TextStyle(
+                    fontSize: 24,
+                    color: AppColors.textColor,
+                  ),
+                ),
+              ),
+              Container(
+                padding: const EdgeInsets.all(15),
+                alignment: Alignment.centerRight,
+                child: Row(
+                  mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                  children: [
+                    if (answer != '')
+                      const Text(
+                        '=',
+                        style: TextStyle(
+                            fontSize: 40,
+                            color: AppColors.blackColor,
+                            fontWeight: FontWeight.bold),
+                      ),
+                    Text(
+                      answer,
+                      style: const TextStyle(
+                          fontSize: 40,
+                          color: AppColors.blackColor,
+                          fontWeight: FontWeight.bold),
+                    ),
+                  ],
+                ),
+              )
+            ],
+          ),
+          Expanded(
+            flex: 5,
+            child: GridView.builder(
+                itemCount: buttons.length,
+                physics: const NeverScrollableScrollPhysics(),
+                gridDelegate: const SliverGridDelegateWithFixedCrossAxisCount(
+                  crossAxisCount: 4,
+                  childAspectRatio: 1.3,
+                ),
+                itemBuilder: (BuildContext context, int index) {
+                  // Clear Button
+                  if (index == 0) {
+                    return MyButton(
+                      buttontapped: () {
+                        setState(() {
+                          userInput = '';
+                          answer = '0';
+                        });
+                      },
+                      buttonText: buttons[index],
+                      color: AppColors.secondaryLightColor,
+                      textColor: AppColors.secondaryColor,
+                    );
+                  }
+
+                  // +/- button
+                  else if (index == 1) {
+                    return MyButton(
+                      buttontapped: () {
+                        setState(() {
+                          userInput =
+                              userInput.substring(0, userInput.length - 1);
+                        });
+                      },
+                      buttonText: buttons[index],
+                      color: AppColors.secondaryLightColor,
+                      textColor: AppColors.secondaryColor,
+                    );
+                  }
+                  // % Button
+                  else if (index == 2) {
+                    return MyButton(
+                      buttontapped: () {
+                        setState(() {
+                          userInput += buttons[index];
+                        });
+                      },
+                      buttonText: buttons[index],
+                      color: AppColors.secondaryLightColor,
+                      textColor: AppColors.secondaryColor,
+                    );
+                  }
+                  // Delete Button
+                  // else if (index == 3) {
+                  //   return MyButton(
+                  //     buttontapped: () {
+                  //       setState(() {
+                  //         userInput =
+                  //             userInput.substring(0, userInput.length - 1);
+                  //       });
+                  //     },
+                  //     buttonText: buttons[index],
+                  //     color: AppColors.secondaryLightColor,
+                  //     textColor: AppColors.secondaryColor,
+                  //   );
+                  // }
+                  // Equal_to Button
+                  else if (index == 19) {
+                    return MyButton(
+                      buttontapped: () {
+                        setState(() {
+                          equalPressed();
+                        });
+                      },
+                      buttonText: buttons[index],
+                      color: AppColors.secondaryColor,
+                      textColor: Colors.white,
+                    );
+                  } else if (index == 16) {
+                    return MyButton(
+                      buttonText: buttons[index],
+                      color: AppColors.whiteLightColor,
+                      textColor: AppColors.textColor,
+                    );
+                  }
+                  //  other buttons
+                  else {
+                    return MyButton(
+                      buttontapped: () {
+                        setState(() {
+                          userInput += buttons[index];
+                        });
+                      },
+                      buttonText: buttons[index],
+                      color: isOperator(buttons[index])
+                          ? AppColors.secondaryLightColor
+                          : AppColors.whiteLightColor,
+                      textColor: isOperator(buttons[index])
+                          ? AppColors.secondaryColor
+                          : AppColors.blackColor,
+                    );
+                  }
+                }),
+          ),
+        ],
+      ),
+    );
+  }
+
+  bool isOperator(String x) {
+    if (x == '/' || x == 'x' || x == '-' || x == '+' || x == '=') {
+      return true;
+    }
+    return false;
+  }
+
+// function to calculate the input operation
+  void equalPressed() {
+    String finaluserinput = userInput;
+    finaluserinput = userInput.replaceAll('x', '*');
+
+    Parser p = Parser();
+    Expression exp = p.parse(finaluserinput);
+    ContextModel cm = ContextModel();
+    double eval = exp.evaluate(EvaluationType.REAL, cm);
+    answer = eval.toString();
+  }
+}
+
+class MyButton extends StatelessWidget {
+  // declaring variables
+  final color;
+  final textColor;
+  final String buttonText;
+  final buttontapped;
+
+  //Constructor
+  const MyButton(
+      {super.key,
+      this.color,
+      this.textColor,
+      required this.buttonText,
+      this.buttontapped});
+
+  @override
+  Widget build(BuildContext context) {
+    return GestureDetector(
+      onTap: buttontapped,
+      child: Padding(
+        padding: const EdgeInsets.all(12),
+        child: ClipRRect(
+          borderRadius: BorderRadius.circular(12),
+          child: Container(
+            color: color,
+            child: Center(
+              child: Text(
+                buttonText,
+                style: TextStyle(
+                  color: textColor,
+                  fontSize: 25,
+                  fontWeight: FontWeight.w500,
+                ),
+              ),
+            ),
+          ),
+        ),
+      ),
+    );
   }
 }
